42c854bdafa8de0cd1d68ea69c97f5ae2bf9baee
Sagar <sagarbhat889@gmail.com>
2025-10-12 07:39:23 -0700

    Http-code added

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -2,8 +2,8 @@ import express from "express"
 import  jwt  from "jsonwebtoken";
 import { JWT_SECRET } from "@repo/backend-common/config";
 import { middleware } from "./middleware";
-
 import {CreateUserSchema} from "@repo/common/types"
+import { Prismaclient } from "@repo/db/client"; 
 const app=express();
 
 
@@ -23,14 +23,31 @@ app.post('signin',function(req,res){
 
 
 
-app.post('signup',function(req,res){
+app.post('signup', async function(req,res){
     
-    const data=CreateUserSchema.safeParse(req.body);
-    if(!data.success){
+    const parsedData=CreateUserSchema.safeParse(req.body);
+    console.log(parsedData)
+    if(!parsedData.success){
         return res.json({
             message:"Incorrect inputs"
         })
     }
+      try {
+        await Prismaclient.user.create({
+        data:{
+            name:parsedData.data.name,
+            email:parsedData.data.email,
+            password:parsedData.data.password
+        }
+       })
+       res.json({
+        message:"User stored"
+       })
+      } catch (error) {
+        res.status(411).json({
+            message:"User already exists with the email"
+        })
+      }
 
     res.json({
         message:"signup room"
--- packages/common/src/types.ts
+++ packages/common/src/types.ts
@@ -1,9 +1,9 @@
 import {z} from "zod";
 
 export const CreateUserSchema=z.object({
-    username:z.string().min(3).max(20),
+    name:z.string().min(3).max(20),
     password:z.string(),
-    name:z.string()
+    email:z.string()
 })
 
 export const SigninSchema=z.object({
